3a182173f71c91d51a26493789f377b64a891337
assout <assout@users.noreply.github.com>
2016-02-10 21:30:32 +0900

    Fix path resolve

--- install.bat
+++ install.bat
@@ -1,4 +1,6 @@
-mklink /J "D:\admin\scripts" "D:\admin\Development\scripts"
+rem 当ファイル自身のパス
+set here=%~dp0
+mklink /J "D:\admin\scripts" "%here%"
 
 git config user.name "assout"
 git config user.email "assout@users.noreply.github.com"
