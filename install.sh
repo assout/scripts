2025fef34ee624c3529eb2145a390e0cb72c5e89
assout <assout@users.noreply.github.com>
2016-02-11 04:04:56 +0900

    Update

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-if [ "${OSTYPE}" = msys ] ; then echo "install.sh: It doesn't meet the prerequisites" >&2 && exit 1 ; fi
+if [ "${OSTYPE}" = msys ] ; then
+  echo "install.sh: It doesn't meet the prerequisites" >&2
+  exit 1
+fi
 
 cd ~/Development
 
