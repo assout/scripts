19ecfa2e47493f47c0bc46353b703f813e1f8240
assout <assout@users.noreply.github.com>
2016-03-17 22:14:43 +0900

    It works

--- snipets/parse_args.sh
+++ snipets/parse_args.sh
@@ -1,3 +1,4 @@
+# short optioin only
 args=""
 isFollow=""
 function parseArgs() {
@@ -14,3 +15,56 @@ function parseArgs() {
   fi
   args=${@}
 }
+
+
+# long option 1 - getoptsとwhileの複合 FIXME: 値付きのがだめかも(-c hoge)
+# http://keens.github.io/blog/2014/02/19/option-parse-for-shell/
+OPTS_RSYNC_OPTS="-aI"
+OPTS_NO_READ_ENV=""
+parseOptions() {
+  while [ "$#" -gt 0 ] ; do
+    arg="${1}" ; shift
+    case "${arg}" in
+      --delete) OPTS_RSYNC_OPTS="${OPTS_RSYNC_OPTS} --delete";;
+      --) break;;
+      --*) ;;
+      -*)
+      OPTIND=1
+      while getopts "c:nvE" OPT "${arg}"; do
+        case $OPT in
+          c) echo ${OPTARG} ;;
+          n) OPTS_RSYNC_OPTS="${OPTS_RSYNC_OPTS} -n" ;;
+          v) OPTS_RSYNC_OPTS="${OPTS_RSYNC_OPTS} -v" ;;
+          E) OPTS_NO_READ_ENV=1 ;;
+          \?) return 1 ;;
+        esac
+      done
+    esac
+  done
+}
+
+# long option 2 - 独自実装
+# Refs: http://qiita.com/b4b4r07/items/dcd6be0bb9c9185475bb
+OPTS_RSYNC_OPTS="-aI"
+OPTS_IS_QUIET=""
+OPTS_CONF_FILE=""
+parse_options() {
+  for OPT in "${@}" ; do 
+    case $OPT in
+      '--delete') OPTS_RSYNC_OPTS="${OPTS_RSYNC_OPTS} --delete" ; shift ;;
+      '--debug') trap 'read -p "${LINENO}: $BASH_COMMAND"' DEBUG; shift ;;
+      '-c')
+        if [[ -z "${2}" ]] || [[ "${2}" =~ ^-+ ]] ; then
+          echo "distribution.sh: option requires an argument -- ${1}" 1>&2
+        fi
+        OPTS_CONF_FILE="${2}"; shift 2 ;;
+      '-h') echo "${USAGE}" 1>&2; return 1 ;;
+      '-n') OPTS_RSYNC_OPTS="${OPTS_RSYNC_OPTS} -n"; shift ;;
+      '-q') OPTS_IS_QUIET="-q"; shift ;;
+      '-v') OPTS_RSYNC_OPTS="${OPTS_RSYNC_OPTS} -v"; shift ;;
+      '--') shift; break ;;
+      -*) (echo "distribution.sh: illegal option -- ${1}"; echo "${USAGE}") 1>&2; return 1;;
+    esac
+  done
+}
+
--- snipets/step_run.sh
+++ snipets/step_run.sh
@@ -1,10 +1,56 @@
 #!/bin/bash
 
-trap 'read -p "$BASH_COMMAND"' DEBUG
+# 基本パターン
+basic() {
+  trap 'read -p "$BASH_COMMAND"' DEBUG
 
-echo 1
+  echo 1
+  echo 2
+  echo 3
+}
 
-echo 2
+# 行番号出力
+lineno() {
+  trap 'read -p "($LINENO) $BASH_COMMAND"' DEBUG
 
-echo 3
+  echo 1
+  echo 2
+}
 
+# function内もステップ実行
+# Refs: http://stackoverflow.com/questions/13223279/how-can-a-sourced-file-inherit-trap-debug
+target() {
+  echo 1
+  echo 2
+}
+
+functrace() {
+  trap 'read -p "($LINENO) $BASH_COMMAND"' DEBUG
+  echo 1st
+  target
+  set -o functrace
+  echo 2nd
+  target
+}
+
+# オプションで指定
+parseOptions() {
+  while [ "$#" -gt 0 ] ; do
+    arg="${1}" ; shift
+    case "${arg}" in
+      --debug) trap 'read -p "$BASH_COMMAND"' DEBUG;;
+      --) break;;
+      --*) ;;
+      -*)
+      OPTIND=1
+      while getopts "nvE" OPT "${arg}"; do
+        case $OPT in
+          n) OPTS_RSYNC_OPTS="${OPTS_RSYNC_OPTS} -n" ;;
+          \?) return 1 ;;
+        esac
+      done
+    esac
+  done
+}
+
+${@}
