1d7c993f2f63fcdb772d16dcb60e7522158362b1
assout <assout@users.noreply.github.com>
2016-03-28 19:08:03 +0900

    Fix symlink

--- symlink.bat
+++ symlink.bat
@@ -1,6 +1,6 @@
 rem 当ファイル自身のパス
 set here=%~dp0
-mklink /J "D:\admin\scripts" "%here%"
+mklink /J "%HOME%\scripts" "%here%"
 
 git config user.name "assout"
 git config user.email "assout@users.noreply.github.com"
